cbe58acf0fbf89de2dc154f6b795e94eb9567add
OgarMikeDev <123175953+OgarMikeDev@users.noreply.github.com>
2025-03-05 14:33:17 +0300

    Update Bot.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -5,20 +5,26 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
-    //Кнопка для запуска тг-бота
-    private InlineKeyboardButton buttonForStartTgBot = InlineKeyboardButton.builder()
-            .text("Нажмите для запуска тг-бота!")
-            .callbackData("запуск")
+    //Кнопка для начала регистрации
+    private InlineKeyboardButton buttonStartRegistration = InlineKeyboardButton.builder()
+            .text("Нажмите для регистрации!")
+            .callbackData("регистрация")
+            .build();
+    //Клавиатура для кнопки для начала регистрации
+    private InlineKeyboardMarkup keyboardForButtonStartRegistration = InlineKeyboardMarkup.builder()
+            .keyboardRow(List.of(buttonStartRegistration))
             .build();
 
-    //Клавиатура для кнопки для запуска тг-бота
-    private InlineKeyboardMarkup keyboardForButtonForStartTgBot = InlineKeyboardMarkup.builder()
-            .keyboardRow(List.of(buttonForStartTgBot))
-            .build();
+    //Переменные для города и ЯП
+    private final String cityForDeveloper = "Сочи";
+    private final String languageProgrammingForDeveloper = "Java";
 
+    //Список для кандидатов в команду разработчиков
+    private ArrayList<User> listSuitableUsers = new ArrayList<>();
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -29,41 +35,62 @@ public class Bot extends TelegramLongPollingBot {
     public void forWorkWithText(Update update) {
         if (update.hasMessage()) {
             String textMessage = update.getMessage().getText();
-            Long idUser = update.getMessage().getFrom().getId();
+            System.out.println("Текст: " + textMessage);
+            String idUser = update.getMessage().getFrom().getId().toString();
 
             SendMessage sendMessage = SendMessage.builder()
                     .chatId(idUser)
                     .text("")
                     .build();
 
-            if (textMessage.equals("/start")) {
-                sendMessage.setText("Вас приветствует тг-бот Огарь М. В.,\n" +
-                        "кот-й поможет найти кандидатов на роль разработчиков\n" +
-                        "в нужном городе и по нужной специальности");
-                sendMessage.setReplyMarkup(keyboardForButtonForStartTgBot);
-            } else if (textMessage.contains("зовут") && textMessage.contains("в городе") &&
-                            textMessage.contains("на языке")) {
-                /*
-                    Меня зовут Михаил.
-                    Я живу в городе Сочи.
-                    Программирую на языке Java.
-                */
-                String templateForName = "зовут ";
-                int leftIndexForName = textMessage.indexOf(templateForName);
-                if (leftIndexForName == -1) {
-                    sendMessage.setText("Ввод имени некорректен :(\n" +
-                            "Повторите снова!");
-                    return;
-                }
-                int rightIndexForName = textMessage.indexOf(".", leftIndexForName + templateForName.length());
-                if (rightIndexForName == -1) {
-                    sendMessage.setText("Ввод имени некорректен :(\n" +
-                            "Повторите снова!");
-                    return;
-                }
-                String name = textMessage.substring(leftIndexForName + templateForName.length(), rightIndexForName);
 
-                System.out.println("Спарсенное имя: " + name);
+            if (textMessage.equals("/start")) {
+                sendMessage.setText("Для регистрации используйте кнопку ниже:");
+                sendMessage.setReplyMarkup(keyboardForButtonStartRegistration);
+            }
+            /*
+            Меня зовут Андрей.
+            Я живу в городе Сочи.
+            Я программирую на языке Java.
+             */
+            else if (textMessage.contains("зовут") && textMessage.contains("в городе") &&
+                    textMessage.contains("на языке")) {
+
+                String templateForName = "зовут ";
+                int leftIndexForName = textMessage.indexOf(templateForName) + templateForName.length(); //индекс буквы А
+                int rightIndexForName = textMessage.indexOf(".", leftIndexForName);
+                String name = textMessage.substring(leftIndexForName, rightIndexForName);
+
+                String templateForCity = "в городе ";
+                int leftIndexForCity = textMessage.indexOf(templateForCity) + templateForCity.length();
+                int rightIndexForCity = textMessage.indexOf(".", leftIndexForCity);
+                String city = textMessage.substring(leftIndexForCity, rightIndexForCity);
+
+                String templateForLanguage = "на языке ";
+                int leftIndexForLanguage = textMessage.indexOf(templateForLanguage) + templateForLanguage.length();
+                int rightIndexForLanguage = textMessage.indexOf(".", leftIndexForLanguage);
+                String language = textMessage.substring(leftIndexForLanguage, rightIndexForLanguage);
+
+                User suitableUser = new User();
+                suitableUser.setId(Long.parseLong(idUser));
+                suitableUser.setName(name);
+                suitableUser.setCity(city);
+                suitableUser.setLanguageProgramming(language);
+
+                System.out.println("Город кандидата: " + suitableUser.getCity() + "\nШаблонный город: " + cityForDeveloper);
+                if (suitableUser.getCity().equals(cityForDeveloper) &&
+                        suitableUser.getLanguageProgramming().equals(languageProgrammingForDeveloper)) {
+                    listSuitableUsers.add(suitableUser);
+                }
+            } else if (textMessage.equals("/list_suitable_users")) {
+                StringBuilder builderForSuitableUsers = new StringBuilder();
+                for (int indexCurrentSuitableUser = 0; indexCurrentSuitableUser < listSuitableUsers.size(); indexCurrentSuitableUser++) {
+                    builderForSuitableUsers.append(listSuitableUsers.get(indexCurrentSuitableUser)).append("\n");
+                }
+
+                System.out.println("Список кандидатов: " + String.valueOf(builderForSuitableUsers));
+                sendMessage.setText(String.valueOf(builderForSuitableUsers));
+
             }
 
             try {
@@ -77,7 +104,7 @@ public class Bot extends TelegramLongPollingBot {
     public void forWorkWithButtons(Update update) {
         if (update.hasCallbackQuery()) {
             String callbackData = update.getCallbackQuery().getData();
-            Long chatId = update.getCallbackQuery().getMessage().getChatId();
+            String chatId = update.getCallbackQuery().getMessage().getChatId().toString();
             Integer messageId = update.getCallbackQuery().getMessage().getMessageId();
 
             EditMessageText editMessageText = EditMessageText.builder()
@@ -86,12 +113,11 @@ public class Bot extends TelegramLongPollingBot {
                     .text("")
                     .build();
 
-            if (callbackData.equals("запуск")) {
-                editMessageText.setText("Напишите текст в следующем формате:\n" +
-                        "\"Меня зовут \'Михаил\'.\n" +
+            if (callbackData.equals("регистрация")) {
+                editMessageText.setText("Сформируйте сообщение в формате\n" +
+                        "\"Меня зовут \'Андрей\'.\n" +
                         "Я живу в городе \'Сочи\'.\n" +
-                        "Программирую на языке \'Java\'." +
-                        "\"");
+                        "Я программирую на языке \'Java\'.\"");
             }
 
             try {
@@ -104,11 +130,11 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "@ogar_search_developers_tg_bot";
+        return "@ogar_online_meetings_tg_bot";
     }
 
     @Override
     public String getBotToken() {
-        return "7885891719:AAF6_2FwCpbsPWuTqNL_MmgG8rcnGrD6lAU";
+        return "7593532456:AAG0hoGZOlhXbAzGFP1HBKkezXKPamQ7qxI";
     }
 }
